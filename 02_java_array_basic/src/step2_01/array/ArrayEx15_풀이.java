51af136dfbd7417781bbe6936c782e85d36dac7b
jhsh <jhsh@218.38.137.27>
2022-10-23 19:46:17 +0900

    두번째 커밋

--- 02_java_array_basic/src/step2_01/array/ArrayEx15_풀이.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx15_풀이.java
@@ -10,16 +10,23 @@ import java.util.Scanner;
  * 2. 정답을 맞추면 back에 해당 숫자를 저장해,
  *    back에 모든 수가 채워지면 게임은 종료된다.
  * 예)
- * front = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+ * front = [5, 4, 1, 3, 1, 2, 4, 2, 3, 5]
  * back  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
- * 입력1 : 0
- * 입력2 : 1
+ * 입력1 : 2
+ * 입력2 : 4
  * 
- * front = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
- * back  = [1, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+ * front = [5, 4, 1, 3, 1, 2, 4, 2, 3, 5]
+ * back  = [0, 0, 1, 0, 1, 0, 0, 0, 0, 0]
+ * 
+ * 입력1 : 5
+ * 입력2 : 7
+ * 
+ * front = [5, 4, 1, 3, 1, 2, 4, 2, 3, 5]
+ * back  = [0, 0, 1, 0, 1, 2, 0, 2, 0, 0]
+ * 
  */
 
-public class ArrayEx15_정답예시 {
+public class ArrayEx15_풀이 {
 
 	public static void main(String[] args) {
 		
@@ -28,42 +35,40 @@ public class ArrayEx15_정답예시 {
 		
 		int[] front = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
 		int[] back = new int[10];
+		int cnt  = 0;
 		
-		// 셔플 구현(Shuffle)
-		for (int i = 0; i <1000; i++) {
+		// 셔플 구현(Shuffle) : 배열의 요소들을 무작위로 섞음
+		while (cnt < 1000) {			
 			int tempRan = ran.nextInt(9) + 1;
 			int temp = front[0]; 
 			front[0] = front[tempRan];
 			front[tempRan] = temp;
+			cnt++;
 		}
-		
-		int answerCnt = 0;
-		while (answerCnt < 5) {
-			
+		int answercnt = 0;
+		while (answercnt < 5) {
 			for (int i = 0; i < front.length; i++) {
 				System.out.print(front[i] + " ");
 			}
 			System.out.println();
-			
-			for (int i = 0; i < back.length; i++) {
+		
+			for (int i = 0; i < front.length; i++) {
 				System.out.print(back[i] + " ");
 			}
 			System.out.println();
-			System.out.print("인덱스 1입력 : ");
-			int idx1 = scan.nextInt();
-			System.out.print("인덱스 2입력 : ");
-			int idx2 = scan.nextInt();
-			
-			
-			if (front[idx1] == front[idx2]) {
-				back[idx1] = front[idx1];
-				back[idx2] = front[idx2];
-				answerCnt++;
+		
+			System.out.println("입력 1: ");
+			int num1 = scan.nextInt()-1;
+			System.out.println("입력 2: ");
+			int num2 = scan.nextInt()-1;
+		
+			if (front[num1] == front[num2]) {
+				back[num1] = front[num1];
+				back[num2] = front[num2];
+				answercnt++;
 			}
-			
 		}
-		
-		System.out.println("게임 종료");
-		
 	}
 }
+
+	
